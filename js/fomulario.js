9a0596d7d14fb0165852857740fc1a44ae9564d6
alanhds <alan.souza@live.com>
2020-04-13 20:07:54 -0300

    faltou o telefone

--- js/fomulario.js
+++ js/fomulario.js
@@ -5,10 +5,12 @@ function display() {
         var form = document.getElementById("formcontact");
         var inputTextNome = form.elements["nome"];
         var inputEmail = form.elements["email"];
+        var inputTelefone = form.elements["telefone"];
         var inputPrioridade = form.elements["prioridade"];
         var inputMsg = form.elements["mensagem"];
         message =  "<ul><li><b>Nome: </b>" + inputTextNome.value;
         message += "<li><b>E-mail: </b>" + inputEmail.value;
+        message += "<li><b>Telefone: </b>" + inputTelefone.value
         message += "<li><b>Prioridade: </b>" + inputPrioridade.value;
         message += "<li><b>Mensagem: </b>" + inputMsg.value + "</ul>";
 
